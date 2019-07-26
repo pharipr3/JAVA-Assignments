29c6052b45b98ccaedcb943b2e32c669ab617b1f
pharipr3 <53307160+pharipr3@users.noreply.github.com>
2019-07-26 20:10:52 +0530

    Create Numbering.java

---
+++ Assignment-1/Numbering.java
@@ -0,0 +1,11 @@
+public class Numbering {
+	public static void main(String[] args) {
+		System.out.print("1 to 100 Number's are printed here\n");
+		int i=1;
+		while(i<101)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
